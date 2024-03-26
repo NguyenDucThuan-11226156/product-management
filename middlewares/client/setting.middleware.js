2e4772f57c0211b1675dd39c98ae4168c0e02a5d
Nguyen Duc Thuan <thuan452004@gmail.com>
2024-03-27 00:17:03 +0700

    update

--- middlewares/client/setting.middleware.js
+++ middlewares/client/setting.middleware.js
@@ -4,6 +4,5 @@ module.exports.settingsGeneral = async (req, res, next) => {
   const settingsGeneral = await SettingGeneral.findOne({});
   console.log(settingsGeneral);
   res.locals.settingsGeneral = settingsGeneral;
-
   next();
 };
